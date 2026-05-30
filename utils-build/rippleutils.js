a79238c80da69ba07f4f6afe44e20a8f2b52a63f
likev <3146974+likev@users.noreply.github.com>
2026-05-30 12:25:00 +0000

    Update ToastWallet dependencies and implement xrpl.js 4.6.0 migration

--- utils-build/rippleutils.js
+++ utils-build/rippleutils.js
@@ -7,9 +7,8 @@ if (typeof window !== 'undefined') {
 	window['rippleAddressCodec'] = require('ripple-address-codec');
 	window['R_B58_DICT'] = 'rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz';
 	window['base58'] = require('base-x')(R_B58_DICT);
-	window['rippleBinaryCodec'] = require('ripple-binary-codec')
-	window['rippleHashes'] = require('ripple-hashes')
 	window['BN'] = require('bn.js');
-	window['hashjs'] = require('hash.js')
-	window['utils'] = require('./utils.js')
+	window['hashjs'] = require('hash.js');
+	window['xrpl'] = require('xrpl');
+	window['utils'] = require('./utils.js');
 }
